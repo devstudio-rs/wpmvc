16b99176b68e147347fedd17bede92b516d986f9
Miloš Petković <milosh.pet@gmail.com>
2024-02-02 00:51:29 +0100

    App class: Move init to main class

--- src/App.php
+++ src/App.php
@@ -2,4 +2,17 @@
 
 namespace wpmvc;
 
-class App extends \wpmvc\base\App {}
+class App extends \wpmvc\base\App {
+
+    /**
+     * @var self
+     */
+    public static $app;
+
+    public static function init( array $config = array() ) {
+        if ( static::$app === null ) {
+            static::$app = new static( $config );
+        }
+    }
+
+}
--- src/base/App.php
+++ src/base/App.php
@@ -4,17 +4,6 @@ namespace wpmvc\base;
 
 abstract class App extends Component {
 
-    /**
-     * @var self
-     */
-    public static $instance;
-
     public function __construct( array $config = array() ) {}
-
-    public static function init( array $config = array() ) {
-        if ( static::$instance === null ) {
-            static::$instance = new static( $config );
-        }
-    }
 
 }
