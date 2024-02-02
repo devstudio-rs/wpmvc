33b7adaee7fca845b9daedc8dee716abc7104fe1
Miloš Petković <milosh.pet@gmail.com>
2024-02-02 20:58:45 +0100

    App: Implement components

--- src/App.php
+++ src/App.php
@@ -2,17 +2,15 @@
 
 namespace wpmvc;
 
-class App extends \wpmvc\base\App {
+abstract class App extends \wpmvc\base\App {
 
     /**
      * @var self
      */
     public static $app;
 
-    public static function init( array $config = array() ) {
-        if ( static::$app === null ) {
-            static::$app = new static( $config );
-        }
+    public function run() {
+        static::$app = $this;
     }
 
 }
--- src/base/App.php
+++ src/base/App.php
@@ -4,6 +4,38 @@ namespace wpmvc\base;
 
 abstract class App extends Component {
 
-    public function __construct( array $config = array() ) {}
+    private $config = array();
+
+    /**
+     * @var Request
+     */
+    public $request;
+
+    public function __construct( array $config = array() ) {
+        $this->set_config( $config );
+
+        $this->setup_components();
+    }
+
+    /**
+     * @param array $config
+     * @return void
+     */
+    private function set_config( array $config = array() ) {
+        $this->config = array_merge( array(
+            'name'       => '',
+            'components' => array(),
+        ), $config );
+    }
+
+    private function setup_components() {
+        if ( empty( $this->config['components'] ) ) {
+            return;
+        }
+
+        foreach ( $this->config['components'] as $component => $params ) {
+            $this->{ $component } = new $params['class']( $params );
+        }
+    }
 
 }
