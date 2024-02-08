177b9d509d1a1342afb40e451e888bfae8c855ac
Milos Petkovic <milosh.pet@gmail.com>
2024-02-08 22:22:26 +0100

    Introduce aliases

--- src/base/App.php
+++ src/base/App.php
@@ -7,7 +7,7 @@ abstract class App extends Component {
     /**
      * @var array
      */
-    private $config = array();
+    protected static $config = array();
 
     /**
      * @var Request
@@ -33,22 +33,43 @@ abstract class App extends Component {
      * @return void
      */
     private function set_config( array $config = array() ) {
-        $this->config = array_merge( array(
+        static::$config = array_merge( array(
             'name'       => '',
+            'aliases'    => array(),
             'components' => array(),
         ), $config );
     }
 
+    /**
+     * Setup application components.
+     *
+     * @return void
+     */
     private function setup_components() {
         $this->router = new \wpmvc\web\Router();
 
-        if ( empty( $this->config['components'] ) ) {
+        if ( empty( static::$config['components'] ) ) {
             return;
         }
 
-        foreach ( $this->config['components'] as $component => $params ) {
+        foreach ( static::$config['components'] as $component => $params ) {
             $this->{ $component } = new $params['class']( $params );
         }
+    }
+
+    public static function alias( $value ) {
+        if ( empty( static::$config['aliases'] ) ) {
+            return $value;
+        }
+
+        $parts = explode( '/', $value );
+        $alias = $parts[0];
+
+        if ( empty( static::$config['aliases'][ $alias ] ) ) {
+            return $value;
+        }
+
+        return str_replace( $alias, static::$config['aliases'][ $alias ], $value );
     }
 
 }
